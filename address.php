03503151487bf05911e41e1f9f2a4475557b2d32
Thomas <33732634+Havenstd06@users.noreply.github.com>
2018-05-12 02:20:21 +0200

    Add download button

---
+++ address.php
@@ -0,0 +1,15 @@
+<?php 
+
+// set the content type
+header('Content-type: text/plain');
+// force save as dialog (and suggest filename)
+header('Content-Disposition: attachment; filename="WtipAddress.txt"');
+// set the variables
+$address = $_POST['address'];
+$privspendkey = $_POST['privspendkey'];
+$privviewkey = $_POST['privviewkey'];
+$seedphrase = $_POST['seedphrase'];
+// print the text
+echo "Address: $address\r\nPrivate Spend Key: $privspendkey\r\nPrivate View Key: $privviewkey\r\nSeed Phrase: $seedphrase";
+
+ ?>
--- assets/js/wallet.js
+++ assets/js/wallet.js
@@ -35,19 +35,24 @@ var restore_keys = function(lang) {
 
     address_widget.innerHTML = keys.public_addr;
     address_widget2.innerHTML = keys.public_addr;
+    address_widget3.value = keys.public_addr;
     address_widget.value = keys.public_addr;
+    address_widget2.value = keys.public_addr;
 
 
     mnemonic_widget.innerHTML = seed_phrase;
     mnemonic_widget2.innerHTML = seed_phrase;
+    mnemonic_widget3.value = seed_phrase;
     mnemonic_widget.value = seed_phrase;
 
     spend_key_widget.innerHTML = keys.spend.sec;
     spend_key_widget2.innerHTML = keys.spend.sec;
+    spend_key_widget3.value = keys.spend.sec;
     spend_key_widget.value = keys.spend.sec;
 
     view_key_widget.innerHTML = keys.view.sec;
     view_key_widget2.innerHTML = keys.view.sec;
+    view_key_widget3.value = keys.view.sec;
     view_key_widget.value = keys.view.sec;
 };
 
@@ -56,12 +61,15 @@ var restore_keys = function(lang) {
 var genwallet = function(lang) {
     var spend_key_widget = document.getElementById("spend_key_widget");
     var spend_key_widget2 = document.getElementById("spend_key_widget2");
+    var spend_key_widget3 = document.getElementById("spend_key_widget3");
 
     var view_key_widget = document.getElementById("view_key_widget");
     var view_key_widget2 = document.getElementById("view_key_widget2");
+    var view_key_widget3 = document.getElementById("view_key_widget3");
 
     var address_widget = document.getElementById("address_widget");
     var address_widget2 = document.getElementById("address_widget2");
+    var address_widget3 = document.getElementById("address_widget3");
 
     var address_qr_widget = document.getElementById("address_qr_widget");
 
@@ -73,18 +81,22 @@ var genwallet = function(lang) {
 
     address_widget.innerHTML = keys.public_addr;
     address_widget2.innerHTML = keys.public_addr;
+    address_widget3.value = keys.public_addr;
     address_widget.value = keys.public_addr;
 
     mnemonic_widget.innerHTML = mnemonic;
     mnemonic_widget2.innerHTML = mnemonic;
+    mnemonic_widget3.value = mnemonic;
     mnemonic_widget.value = mnemonic;
 
     spend_key_widget.innerHTML = keys.spend.sec;
     spend_key_widget2.innerHTML = keys.spend.sec;
+    spend_key_widget3.value = keys.spend.sec;
     spend_key_widget.value = keys.spend.sec;
 
     view_key_widget.innerHTML = keys.view.sec;
     view_key_widget2.innerHTML = keys.view.sec;
+    view_key_widget3.value = keys.view.sec;
     view_key_widget.value = keys.view.sec;
 
     var typeNumber = 0;
