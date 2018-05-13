8b0db37edd504b60ff8a1f8fbbc3871ca192d6f0
Thomas Drumont <me@dru.wtf>
2018-05-13 12:15:20 +0200

    Update

--- address.php
+++ address.php
@@ -1,5 +1,4 @@
 <?php 
-
 // set the content type
 header('Content-type: text/plain');
 // force save as dialog (and suggest filename)
@@ -10,6 +9,6 @@ $privspendkey = $_POST['privspendkey'];
 $privviewkey = $_POST['privviewkey'];
 $seedphrase = $_POST['seedphrase'];
 // print the text
-echo "Address: $address\r\nPrivate Spend Key: $privspendkey\r\nPrivate View Key: $privviewkey\r\nSeed Phrase: $seedphrase";
+echo "Address: $address\r\nPrivate Spend Key: $privspendkey\r\nPrivate View Key: $privviewkey\r\nSeed Phrase: $seedphrase\r\n\r\nThank you for using the WorkTips Wallet Generator!\r\nhttps://wtipwallet.havens.space/";
 
  ?>
